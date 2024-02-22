15fea03faf1ddb7d0468e61aad20c0b3fd8507c1
Purcariu Vlad <purcariuvlad@Purcarius-Mac-mini.local>
2024-02-22 15:49:22 +0100

    added animation after completing contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Contact.css";
 import {
@@ -10,6 +9,8 @@ import {
   FaInstagram,
   FaDiscord,
 } from "react-icons/fa";
+import Lottie from "lottie-react";
+import animationData from "../../assets/Animation - 1708611839942.json";
 import emailjs from "@emailjs/browser";
 import Layout from "../../components/Layout/Layout";
 
@@ -17,40 +18,39 @@ const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-      // EMAILJS
 
-  const serviceId = import.meta.env.VITE_SERVICE_ID;
-  const templateId =import.meta.env.VITE_TEMPLATE_ID;
-  const publicKey = import.meta.env.VITE_PUBLIC_EMAIL_KEY;
+    const serviceId = import.meta.env.VITE_SERVICE_ID;
+    const templateId = import.meta.env.VITE_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_PUBLIC_EMAIL_KEY;
 
-  // TEMPLATE PARAMS
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "UnifyFootball",
+      message: message,
+    };
 
-  const templateParams = {
-    from_name: name,
-    from_email: email,
-    to_name: "UnifyFootball",
-    message: message,
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("Email sent successfully!", response);
+        setSuccess(true);
+        setError(false);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.log("Error sending email: ", error);
+        setSuccess(false);
+        setError(true);
+      });
   };
-   // SEND EMAIL using EMAILJS
-   emailjs
-   .send(serviceId, templateId,templateParams, publicKey)
-   .then((response) => {
-     console.log("Email sent successfully!", response);
-     setName("");
-     setEmail("");
-     setMessage("");
-   })
-   .catch((error) => {
-     console.log("Error sending email: ", error);
-   });
-  };
-
-
-
- 
 
   useEffect(() => {
     const inputs = document.querySelectorAll(".input");
@@ -147,49 +147,63 @@ const Contact = () => {
               <span className="circle one"></span>
               <span className="circle two"></span>
 
-              <form onSubmit={handleSubmit} autoComplete="off">
-                <h3 className="title">Contactează-ne</h3>
-                <div className="input-container">
-                  <input
-                    type="text"
-                    name="name"
-                    value={name}
-                    className="input"
-                    required
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                  <label htmlFor="">Username</label>
-                  <span>Username</span>
+              {success ? (
+                <div className="success-message">
+                    <Lottie animationData={animationData}></Lottie>
+                  <p className="success-txt scs-l">Form submitted successfully!</p>
+                  <p className="success-txt scs-r">We will contact you shortly!</p>
                 </div>
-                <div className="input-container">
-                  <input
-                    type="email"
-                    name="email"
-                    value={email}
-                    className="input"
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  <label htmlFor="">Email</label>
-                  <span>Email</span>
+              ) : (
+                <form onSubmit={handleSubmit} autoComplete="off">
+                  <h3 className="title">Contactează-ne</h3>
+                  <div className="input-container">
+                    <input
+                      type="text"
+                      name="name"
+                      value={name}
+                      className="input"
+                      required
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                    <label htmlFor="">Username</label>
+                    <span>Username</span>
+                  </div>
+                  <div className="input-container">
+                    <input
+                      type="email"
+                      name="email"
+                      value={email}
+                      className="input"
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <label htmlFor="">Email</label>
+                    <span>Email</span>
+                  </div>
+                  <div className="input-container">
+                    <input type="tel" name="phone" className="input" required />
+                    <label htmlFor="">Phone</label>
+                    <span>Phone</span>
+                  </div>
+                  <div className="input-container textarea">
+                    <textarea
+                      name="message"
+                      value={message}
+                      className="input"
+                      onChange={(e) => setMessage(e.target.value)}
+                    ></textarea>
+                    <label htmlFor="">Message</label>
+                    <span>Message</span>
+                  </div>
+                  <input type="submit" value="Send" className="btn" />
+                </form>
+              )}
+
+              {error && (
+                <div className="error-message">
+                  <p>Error submitting the form. Please try again later.</p>
                 </div>
-                <div className="input-container">
-                  <input type="tel" name="phone" className="input" required />
-                  <label htmlFor="">Phone</label>
-                  <span>Phone</span>
-                </div>
-                <div className="input-container textarea">
-                  <textarea
-                    name="message"
-                    value={message}
-                    className="input"
-                    onChange={(e) => setMessage(e.target.value)}
-                  ></textarea>
-                  <label htmlFor="">Message</label>
-                  <span>Message</span>
-                </div>
-                <input type="submit" value="Send" className="btn" />
-              </form>
+              )}
             </div>
           </div>
         </div>
